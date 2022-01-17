e4b5e2ab8f00538106647f908ffe43990d9aadde
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-01-17 23:56:50 +0100

    refactor: Code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fmt::Debug;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
 use thread_timer::ThreadTimer;
 
@@ -19,20 +20,20 @@ where
     let items = Arc::new(Mutex::new(Vec::new()));
     let (buffered_tx, buffered_rx) = channel::<Vec<T>>();
 
-    let res = items.clone();
-    std::thread::spawn(move || loop {
+    let cloned_items = items.clone();
+    thread::spawn(move || loop {
         let _ = timer_rx.recv();
         let _ = timer.cancel();
-        let r = res.clone();
+        let items = cloned_items.clone();
         let bx = buffered_tx.clone();
         let _ = timer.start(cooldown_time, move || {
-            bx.send(r.lock().unwrap().clone()).unwrap();
-            println!("[[-]]: flushing with {:?}", r.lock().unwrap());
-            r.lock().unwrap().clear();
+            bx.send(items.lock().unwrap().clone()).unwrap();
+            println!("[[-]]: flushing with {:?}", items.lock().unwrap());
+            items.lock().unwrap().clear();
         });
     });
 
-    std::thread::spawn(move || loop {
+    thread::spawn(move || loop {
         if let Ok(num) = item_rx.recv() {
             println!("[[<<]]: get num: {:?}", num);
             timer_tx.send(StartTimerMsg).unwrap();
