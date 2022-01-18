a501447e0621aa866cb35038aa6b73435dcf7197
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-01-18 20:14:17 +0100

    test: Add high level integration test

---
+++ tests/integration_tests.rs
@@ -0,0 +1,43 @@
+use cooldown_buffer::cooldown_buffer;
+use std::thread;
+use std::time::Duration;
+
+#[test]
+fn it_works() {
+    // given
+    let (tx, rx) = cooldown_buffer(Duration::from_millis(100));
+
+    // when
+    tx.send(1).unwrap();
+    thread::sleep(Duration::from_millis(90));
+    tx.send(2).unwrap();
+    thread::sleep(Duration::from_millis(90));
+    tx.send(3).unwrap();
+
+    thread::sleep(Duration::from_millis(110)); // cooled down -> first buffer
+
+    tx.send(4).unwrap();
+
+    thread::sleep(Duration::from_millis(110)); // cooled down -> second buffer
+
+    tx.send(5).unwrap();
+    thread::sleep(Duration::from_millis(90));
+    tx.send(6).unwrap();
+
+    thread::sleep(Duration::from_millis(110)); // cooled down -> third buffer
+
+    let buf1 = rx.recv().unwrap();
+    let buf2 = rx.recv().unwrap();
+    let buf3 = rx.recv().unwrap();
+
+    tx.send(7).unwrap();
+    tx.send(8).unwrap();
+    // cool down time didn't passs yet, so we won'thave items in rx
+    let res = rx.try_recv();
+
+    // then
+    assert_eq!(buf1, vec![1, 2, 3]);
+    assert_eq!(buf2, vec![4]);
+    assert_eq!(buf3, vec![5, 6]);
+    assert!(res.is_err());
+}
